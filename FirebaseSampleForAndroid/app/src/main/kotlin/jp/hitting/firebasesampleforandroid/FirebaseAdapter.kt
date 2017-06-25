8c57ea3c4cbedce5a3b24ac207d0167f6eea87fa
IOKA <hit0apps@gmail.com>
2017-06-25 23:34:19 +0900

    fix cast

--- FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/FirebaseAdapter.kt
+++ FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/FirebaseAdapter.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
-import java.util.*
 
 class FirebaseAdapter(context: Context, textViewResourceId: Int, private val items: MutableList<DataSnapshot>) : ArrayAdapter<DataSnapshot>(context, textViewResourceId) {
 
@@ -22,7 +21,7 @@ class FirebaseAdapter(context: Context, textViewResourceId: Int, private val ite
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val user = this.items[position].value as Dictionary<String, String>
+        val user = this.items[position].value as Map<String, String>
         val view = this.inflater.inflate(android.R.layout.simple_list_item_1, null)
         val text1 = view.findViewById(android.R.id.text1) as TextView
         text1.text = user["name"] + ": " + user["address"]
