706525d9a7f8f9d831693dfb619e0cdb36662ab5
IOKA <hit0apps@gmail.com>
2017-07-03 22:37:38 +0900

    model for firebase

---
+++ FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/User.kt
@@ -0,0 +1,8 @@
+package jp.hitting.firebasesampleforandroid
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+class User(val name: String, val address: String) {
+
+}
