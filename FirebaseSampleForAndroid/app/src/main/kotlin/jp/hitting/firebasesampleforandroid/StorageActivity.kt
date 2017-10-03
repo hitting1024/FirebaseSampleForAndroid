b0034c93db84c0adf5dd09af4b7a05e888b39319
IOKA <hit0apps@gmail.com>
2017-10-03 23:23:50 +0900

    convert to kotlin

---
+++ FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/StorageActivity.kt
@@ -0,0 +1,13 @@
+package jp.hitting.firebasesampleforandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class StorageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storage)
+    }
+
+}
