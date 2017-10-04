0856ec374adb874674f40ba8492d305fa73b9d51
IOKA <hit0apps@gmail.com>
2017-10-04 22:57:38 +0900

    init layout

--- FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/StorageActivity.kt
+++ FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/StorageActivity.kt
@@ -2,12 +2,23 @@ package jp.hitting.firebasesampleforandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 
 class StorageActivity : AppCompatActivity() {
 
+    private var imageView1: ImageView? = null
+    private var imageView2: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_storage)
+
+        this.initLayout()
+    }
+
+    private fun initLayout() {
+        this.setContentView(R.layout.activity_storage)
+        this.imageView1 = this.findViewById(R.id.image1) as ImageView
+        this.imageView2 = this.findViewById(R.id.image2) as ImageView
     }
 
 }
