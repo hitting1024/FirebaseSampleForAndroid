079aaf10e4e83f5a6972af11f5657977d2ae8122
IOKA <hit0apps@gmail.com>
2017-10-05 21:37:15 +0900

    init firebase storage

--- FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/StorageActivity.kt
+++ FirebaseSampleForAndroid/app/src/main/kotlin/jp/hitting/firebasesampleforandroid/StorageActivity.kt
@@ -3,8 +3,13 @@ package jp.hitting.firebasesampleforandroid
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
 
 class StorageActivity : AppCompatActivity() {
+
+    private var storageRef: StorageReference? = null
 
     private var imageView1: ImageView? = null
     private var imageView2: ImageView? = null
@@ -13,12 +18,17 @@ class StorageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         this.initLayout()
+        this.initFirebase()
     }
 
     private fun initLayout() {
         this.setContentView(R.layout.activity_storage)
         this.imageView1 = this.findViewById(R.id.image1) as ImageView
         this.imageView2 = this.findViewById(R.id.image2) as ImageView
+    }
+
+    private fun initFirebase() {
+        this.storageRef = FirebaseStorage.getInstance().reference
     }
 
 }
